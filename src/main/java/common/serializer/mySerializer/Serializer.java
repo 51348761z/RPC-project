8b9109bffe928ea2648ae51b2504bde3c96c644c
archwongs <hl_wong@st.gxu.edu.cn>
2025-07-22 13:30:06 +0800

    refactor: improve naming and remove comments in Serializer interface
    - Rename getSerializerByCode to createSerializer for better clarity
    - Remove redundant comments to reduce visual clutter

--- src/main/java/common/serializer/mySerializer/Serializer.java
+++ src/main/java/common/serializer/mySerializer/Serializer.java
@@ -1,16 +1,11 @@
 package common.serializer.mySerializer;
 
 public interface Serializer {
-    // serialize an object to a byte array
     byte[] serialize(Object object);
-
-    // deserialize a byte array to an object of type T
     Object deserialize(byte[] bytes, int messageType);
-
-    // get the type of the serializer
     SerializerType getType();
 
-    static Serializer getSerializerByCode(SerializerType type) {
+    static Serializer createSerializer(SerializerType type) {
         return switch (type) {
             case OBJECT_SERIALIZER -> new ObjectSerializer();
             case JSON_SERIALIZER -> new JsonSerializer();
