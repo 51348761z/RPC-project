d939b86cebdb26f184195213f0e02ca1a8f25850
archwongs <hl_wong@st.gxu.edu.cn>
2025-07-28 20:44:50 +0800

    feat(client): implement circuit breaker pattern for RPC service protection
    - Add CircuitBreaker with CLOSED/OPEN/HALF_OPEN states and configurable thresholds
    - Integrate CircuitBreakerProvider in ClientProxy to protect method calls
    - Update TestClient with concurrent testing for circuit breaker validation

--- src/main/java/Client/TestClient.java
+++ src/main/java/Client/TestClient.java
@@ -7,16 +7,29 @@ import common.service.UserService;
 public class TestClient {
     public static void main(String[] args) throws InterruptedException {
         ClientProxy clientProxy = new ClientProxy();
-        UserService userServiceNetty = clientProxy.getProxy(UserService.class);
+        UserService proxy = clientProxy.getProxy(UserService.class);
 
-        User user = userServiceNetty.getUserById(1);
-        User u = User.builder()
-                .id(100)
-                .username("xxx")
-                .sex(true)
-                .build();
-        Integer id = userServiceNetty.insertUserId(u);
-        System.out.println("--- Using Netty Client Proxy ---");
-
+        for (int i = 0; i < 120; i++) {
+            Integer userId = i;
+            if (i % 30 == 0) {
+                Thread.sleep(10000);
+            }
+            new Thread(()->{
+                try {
+                    User user = proxy.getUserById(userId);
+                    System.out.println("User ID: " + userId + ", User: " + user + " from thread: " + Thread.currentThread().getName());
+                    Integer id = proxy.insertUserId(User.builder()
+                            .id(userId)
+                            .username("User" + userId.toString())
+                            .sex(true)
+                            .build());
+                    System.out.println("Inserted User ID: " + id + " from thread: " + Thread.currentThread().getName());
+                } catch (NullPointerException e) {
+                    System.out.println("Service not available for User ID: " + userId + " from thread: " + Thread.currentThread().getName());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }).start();
+        }
     }
 }
